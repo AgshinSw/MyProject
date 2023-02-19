a3d04ed9ef33d27f17476448362b4c99208a396b
Aqsin Elekberli <aqsin.elekberli@gmail.com>
2023-02-19 20:15:37 +0400

    19_02_2023 20:15

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class MyDictionary<TKey, Tvalue>
+    {
+
+        TKey[] keyarray, tempkeyarray;
+        Tvalue[] valuearray, tempvaluearray;
+
+        public MyDictionary()
+        {
+            keyarray = new TKey[0];
+            valuearray = new Tvalue[0];
+
+        }
+
+
+
+        public void Add(TKey key, Tvalue value)
+        {
+            tempkeyarray = keyarray;
+            tempvaluearray = valuearray;
+
+            keyarray = new TKey[keyarray.Length + 1];
+            valuearray = new Tvalue[valuearray.Length + 1];
+
+            for (int i = 0; i < tempkeyarray.Length; i++)
+            {
+                keyarray[i] = tempkeyarray[i];
+            }
+
+            for (int i = 0; i < tempvaluearray.Length; i++)
+            {
+                valuearray[i] = tempvaluearray[i];
+            }
+
+            keyarray[keyarray.Length - 1] = key;
+            valuearray[valuearray.Length - 1] = value;
+
+            Console.WriteLine(keyarray[keyarray.Length - 1]);
+            Console.WriteLine(valuearray[valuearray.Length - 1]);
+
+        }
+
+
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> numbername = new MyDictionary<int, string>();
+            numbername.Add(1, "One");
+            numbername.Add(2, "Two");
+            numbername.Add(3, "Three");
+        }
+    }
+}
