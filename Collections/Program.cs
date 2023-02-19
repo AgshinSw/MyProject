0763967533a5719a2c1acaead617d82cc2e826da
Aqsin Elekberli <aqsin.elekberli@gmail.com>
2023-02-19 20:29:32 +0400

    19022023

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -10,7 +10,7 @@ namespace Collections
             //Console.WriteLine("Hello World!");
             List<string> names = new List<string> { "Name1", "Name2", "Name3", "Name4"};
             //names.Remove("Name1");
-            names.Add("salam");
+            names.Add("Hello");
 
 
             foreach (var item in names)
